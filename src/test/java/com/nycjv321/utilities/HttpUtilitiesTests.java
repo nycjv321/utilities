73336cfd6224ca83e874f07ffdc49ada082d0a1b
Javier <nycjv321@gmail.com>
2014-08-31 17:20:29 -0400

    HttpUtilities Unit Tests
    Added HttpUtilities Unit Tests

--- src/test/java/com/nycjv321/utilities/HttpUtilitiesTests.java
+++ src/test/java/com/nycjv321/utilities/HttpUtilitiesTests.java
@@ -1,7 +1,23 @@
 package com.nycjv321.utilities;
 
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
+
 /**
  * Created by Javier on 8/31/2014.
  */
 public class HttpUtilitiesTests {
+
+    @Test
+    public void test200Head() {
+        assertEquals(HttpUtilities.head(HttpUtilities.createURI("http://www.google.com")).getStatusLine().getStatusCode(), 200);
+    }
+
+    @Test
+    public void testGet() {
+        assertNotNull(HttpUtilities.get(HttpUtilities.createURI("http://www.google.com")));
+    }
+
 }
