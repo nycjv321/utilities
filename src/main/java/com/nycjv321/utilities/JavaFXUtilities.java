0c4434cabb3806719049672dd48c25aa6b9a749d
Javier <nycjv321@gmail.com>
2014-09-08 23:15:18 -0400

    Added new methods to JavaFXUtilities

--- src/main/java/com/nycjv321/utilities/JavaFXUtilities.java
+++ src/main/java/com/nycjv321/utilities/JavaFXUtilities.java
@@ -1,10 +1,13 @@
 package com.nycjv321.utilities;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Accordion;
-import javafx.scene.control.Label;
-import javafx.scene.control.TitledPane;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Font;
 
 /**
  * Holds utility methods for interacting with JavaFX UI Form Controls
@@ -29,10 +32,80 @@ public interface JavaFXUtilities {
     }
 
     public default Label createLabel(String text, int width, Pos position) {
+        return createLabel(text, width, position);
+    }
+
+    public default Label createLabel(String text, int width, Pos position, Font font) {
+        return createLabel(text, width, 0, position, font);
+    }
+
+    public default Label createLabel(int width, int height, Pos position) {
+        return createLabel(null, width, height, position, null);
+    }
+
+
+    public default Label createLabel(String text, int width, int height, Pos position) {
+        return createLabel(text, width, height, position);
+    }
+
+    public default Label createLabel(String text, int width, int height, Pos position, Font font) {
         Label label = new Label(text);
-        label.setPrefWidth(width);
+        if (width != 0)
+            label.setPrefWidth(width);
+        if (height != 0)
+            label.setPrefWidth(height);
         label.setAlignment(position);
+        if (font != null)
+            label.setFont(font);
         return label;
+    }
+
+
+    public default Button createButton(String text, EventHandler<ActionEvent> event) {
+        return createButton(text, null, 0, event);
+    }
+
+    public default Button createButton(String text, Pos alignment, int preferredWidth, EventHandler<ActionEvent> event) {
+        Button button = new Button();
+        button.setText(text);
+        if (alignment != null)
+            button.setAlignment(alignment);
+        if (preferredWidth != 0)
+            button.setPrefWidth(preferredWidth);
+        button.setOnAction(event);
+        return button;
+    }
+
+    public default Image createImage(String resourcePath) {
+        return new Image(getClass().getResourceAsStream(resourcePath));
+
+    }
+
+    public default void setToolTip(Control control, String value) {
+        control.setTooltip(new Tooltip(value));
+    }
+
+    public default CheckBox createCheckBox(String caption, int width, Pos position, boolean selected) {
+        CheckBox checkBox = new CheckBox();
+        checkBox.setText(caption);
+        checkBox.setAlignment(position);
+        checkBox.setPrefWidth(width);
+        checkBox.setSelected(selected);
+        return checkBox;
+    }
+
+    public default ImageView createImageView(Image image, int fitHeight, boolean preserveRatio) {
+        ImageView value = new ImageView();
+        value.setFitHeight(fitHeight);
+        value.setPreserveRatio(preserveRatio);
+        value.setImage(image);
+        return value;
+    }
+
+    public default TextField createTextField(Pos position) {
+        TextField textField = new TextField();
+        textField.setAlignment(position);
+        return textField;
     }
 
 }
